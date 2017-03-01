e92e3a8fdec9af226aecfdfb62316d81f0ad5d04
masturiano <mydelaraasturiano@gmail.com>
2017-03-01 23:28:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="head">
+			<div id="website-name"></div>
+		</div>
+		<div id="nav"></div>
+		<div id="content"></div>
+		<div id="footer"></div>
+	</body>
+</html>
